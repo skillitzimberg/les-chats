80b3e8cdcab9a0f5d6383928a582b19b968cbc25
Scott Bergler <commandinghands@gmail.com>
2022-01-20 05:20:13 -0800

    Rename repository

--- server/repository.go
+++ server/repository.go
@@ -1,26 +1,35 @@
 package main
 
-import "gorm.io/gorm"
+import (
+	"fmt"
 
-type Repo struct {
+	"gorm.io/gorm"
+)
+
+type Repository struct {
 	db    *gorm.DB
 	users *[]user
 }
 
-func NewRepo(db *gorm.DB) *Repo {
+func NewRepo(db *gorm.DB) *Repository {
 	users := []user{}
-	return &Repo{db, &users}
+	return &Repository{db, &users}
 }
 
-func (r *Repo) MigrateSchema() {
+func (r *Repository) MigrateSchema() {
 	r.db.AutoMigrate(&user{}, &message{})
 }
 
-func (r *Repo) CreateUser(u *user) error {
+func (r *Repository) CreateUser(u *user) error {
 	return r.db.Create(u).Error
 }
 
-func (r *Repo) GetUsers() (*[]user, error) {
+func (r *Repository) GetUsers() (*[]user, error) {
 	err := r.db.Find(&r.users).Error
 	return r.users, err
+}
+
+func (r *Repository) GetUserByUsername(usr *user, username string) error {
+	fmt.Println("GetUserByUsername", usr.Username, username)
+	return r.db.Where("username = ?", username).First(&usr).Error
 }
