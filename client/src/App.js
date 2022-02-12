055d7e7742649f614722c9cb6c9f8ed5ea9ed93e
Scott Bergler <commandinghands@gmail.com>
2022-02-12 07:20:19 -0800

    Move chat components and state to Home component; use token expiration in place of PrivateRoute component

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
+import { tokenIsExpired } from "./Components/auth-utils";
 import ChatForm from "./Components/ChatForm";
+import Home from "./Components/Home";
 import Login from "./Components/Login";
 import Logout from "./Components/Logout";
 import Messages from "./Components/Messages";
@@ -11,27 +13,7 @@ import Registration from "./Components/Registration";
 import Users from "./Components/Users";
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [messages, setMessages] = useState([]);
-  const [currentUser, setCurrentUser] = useState(
-    JSON.parse(localStorage.getItem("currentUser"))
-  );
-
-  useEffect(() => {
-    const loadUsers = () => {
-      fetch("/api/users")
-        .then((resp) => resp.json())
-        .then((usrsData) => setUsers(usrsData));
-    };
-    const loadMessages = () => {
-      fetch("/api/messages")
-        .then((resp) => resp.json())
-        .then((usrsData) => setMessages(usrsData));
-    };
-
-    loadUsers();
-    loadMessages();
-  }, []);
+  const [tokenIsValid, setTokenIsValid] = useState(!tokenIsExpired());
 
   async function handleRegistration(username, password) {
     const response = await fetch("/api/users/register", {
@@ -54,39 +36,6 @@ function App() {
     }
   }
 
-  async function handleLogin(user = null) {
-    const response = await fetch("/api/users/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
-    });
-
-    user.isLoggedIn = response.ok;
-    localStorage.setItem("currentUser", JSON.stringify(user));
-    setCurrentUser(user);
-    if (response.ok) window.location.replace("/");
-    return response.ok;
-  }
-
-  function handleLogout() {
-    currentUser.isLoggedIn = false;
-    localStorage.setItem("currentUser", JSON.stringify(currentUser));
-    setCurrentUser(currentUser);
-    window.location.replace("/login");
-  }
-
-  function handleNewMessage(message) {
-    const newMessage = {
-      id: messages.length + 1,
-      from: currentUser.username,
-      text: message,
-      timeStamp: Date.now(),
-    };
-    setMessages([...messages, newMessage]);
-  }
-
   return (
     <main className="App">
       <Router>
@@ -95,20 +44,14 @@ function App() {
             path="/register"
             element={<Registration handleRegistration={handleRegistration} />}
           />
-          <Route path="/login" element={<Login handleLogin={handleLogin} />} />
           <Route
             path="/"
             element={
-              <PrivateRoute isLoggedIn={currentUser?.isLoggedIn}>
-                <section id="sidebar">
-                  <Users users={users} />
-                </section>
-                <section id="chats">
-                  <Messages messages={messages} />
-                  <ChatForm handleNewMessage={handleNewMessage} />
-                  <Logout logout={handleLogout} />
-                </section>
-              </PrivateRoute>
+              !tokenIsValid ? (
+                <Login setTokenIsValid={setTokenIsValid} />
+              ) : (
+                <Home setTokenIsValid={setTokenIsValid} />
+              )
             }
           />
         </Routes>
