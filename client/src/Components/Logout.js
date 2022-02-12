48ee0be3755c8d55351f0b2390f28376b7d361b1
Scott Bergler <commandinghands@gmail.com>
2022-02-12 07:27:40 -0800

    Ignore deve tools

--- client/src/Components/Logout.js
+++ client/src/Components/Logout.js
@@ -1,9 +1,16 @@
 import React from "react";
-
 import { logout } from "./auth-utils";
 
-const Logout = () => {
-  return <button onClick={logout}>Logout</button>;
+const Logout = ({ setTokenIsValid }) => {
+  return (
+    <button
+      onClick={() => {
+        logout(), setTokenIsValid(false);
+      }}
+    >
+      Logout
+    </button>
+  );
 };
 
 export default Logout;
