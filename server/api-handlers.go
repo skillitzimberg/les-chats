e4a5a214ece48e64a918626a51911dc0cc8fe9e0
Scott Bergler <commandinghands@gmail.com>
2022-02-15 06:08:13 -0800

    Remove unused functions

--- server/api-handlers.go
+++ server/api-handlers.go
@@ -118,24 +118,6 @@ func (api *API) getUsers(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(users)
 }
 
-// func (api *API) getUser(w http.ResponseWriter, r *http.Request) {
-// 	var user user
-// 	vars := mux.Vars(r)
-// 	id, err := strconv.Atoi(vars["id"])
-// 	if err != nil {
-// 		json.NewEncoder(w).Encode(fmt.Sprintf("Invalid ID: %v", vars["id"]))
-// 		return
-// 	}
-// 		for _, usr := range users {
-// 			if id > 0 && usr.Id == id {
-// 				user = usr
-// 				json.NewEncoder(w).Encode(user)
-// 				return
-// 			}
-// 		}
-// 		json.NewEncoder(w).Encode(fmt.Sprintf("User %v not found", id))
-// }
-
 // func (api *API) createMessage(w http.ResponseWriter, r *http.Request) {
 // 	var newMessage message
 // 	reqBody, err := ioutil.ReadAll(r.Body)
@@ -151,32 +133,11 @@ func (api *API) getUsers(w http.ResponseWriter, r *http.Request) {
 // 	json.NewEncoder(w).Encode(messages)
 // }
 
-// func (api *API) getMessage(w http.ResponseWriter, r *http.Request) {
-// 	var message message
-// 	vars := mux.Vars(r)
-// 	id, err := strconv.Atoi(vars["id"])
-// 	if err != nil {
-// 		json.NewEncoder(w).Encode(fmt.Sprintf("Invalid ID: %v", vars["id"]))
-// 		return
-// 	}
-// 	for _, msg := range messages {
-// 		if msg.Id == id {
-// 			message = msg
-// 			json.NewEncoder(w).Encode(message)
-// 			return
-// 		}
-// 	}
-// 		json.NewEncoder(w).Encode(fmt.Sprintf("Message %v not found", id))
-// }
-
 func (api *API) registerEndpoints() {
 	router.HandleFunc("/api/users/ping", api.ping).Methods("POST")
 	router.HandleFunc("/api/users/register", api.register).Methods("POST")
 	router.HandleFunc("/api/users/login", api.login).Methods("POST")
 	router.HandleFunc("/api/users", IsAuthorized(api.getUsers)).Methods("GET")
-	// router.HandleFunc("/api/users/{id}", api.getUser).Methods("GET")
 
 	// router.HandleFunc("/api/messages", api.createMessage).Methods("POST")
-	// router.HandleFunc("/api/messages", api.getMessages).Methods("GET")
-	// router.HandleFunc("/api/messages/{id}", api.getMessage).Methods("GET")
 }
